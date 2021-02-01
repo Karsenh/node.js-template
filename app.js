74f48c7535efff401b76314959e5ab475e527306
Karsenh <KarsenHansen@gmail.com>
2021-01-31 22:23:31 -0800

    Added Mongoose connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -14,10 +15,15 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb//localhost:27017/<DB_NAME>", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 // TODO:
 
 // Process.env.port for Heroku remote hosting
 // 3000 for local hosting / testing
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Successfully started server!");
 });
